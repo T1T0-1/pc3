c6778fd4d98c74c5c85f7e73e314fc5b4ae9fcd4
T1T0-1 <u24201402@utp.edu.pe>
2025-11-10 18:16:21 -0500

    Actualizar prueba.java

--- algoritmo/prueba.java
+++ algoritmo/prueba.java
@@ -152,21 +152,5 @@ public class QuickSortGUI extends JFrame {
 }
 
 
-/////////
-// MÉTODOS DE BASE DE DATOS (.TXT)
-    // ======================================================
-
-    // Cargar datos desde archivo .txt
-    private void cargarDesdeArchivo() {
-        File archivo = new File(ARCHIVO);
-        if (!archivo.exists()) {
-       txtResultado.setText("No se encontró el archivo de datos. Se creará automáticamente.\n");
-            return;
-        }
-        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
-            String linea = br.readLine();
-            if (linea != null && !linea.isEmpty()) {
-                txtNumeros.setText(linea.trim());
-                txtResultado.setText("Datos cargados desde archivo TXT:\n" + linea + "\n");
-            } else {
+//
                 
